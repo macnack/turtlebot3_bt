5db8898aecc2a6227ca5b8a3896aec240c06d37c
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-11-19 00:01:29 +0100

    feat: add homework
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- bt_scripts/behaviours.py
+++ bt_scripts/behaviours.py
@@ -10,44 +10,90 @@ import rclpy.qos
 import std_msgs.msg as std_msgs
 import geometry_msgs.msg as geometry_msgs
 
-class Rotate(py_trees.behaviour.Behaviour):
-	def __init__(self, 
-				 name: str,
-				 topic_name: str="/cmd_vel",
-				 counterclockwise: bool=False,
-				 vel: float=0.2):
-		super(Rotate, self).__init__(name=name)
-		self.topic_name = topic_name
-		self.counterclockwise = counterclockwise
-		self.vel = vel
 
-	def setup(self, **kwargs):
-		self.logger.debug("{}.setup()".format(self.qualified_name))
-		try:
-			self.node = kwargs['node']
-		except KeyError as e:
-				error_message = "didn't find 'node' in setup's kwargs [{}][{}]".format(self.qualified_name)
-				raise KeyError(error_message) from e  # 'direct cause' traceability
-		
-		self.publisher = self.node.create_publisher(
+class Rotate(py_trees.behaviour.Behaviour):
+    def __init__(self,
+                 name: str,
+                 topic_name: str = "/cmd_vel",
+                 counterclockwise: bool = False,
+                 vel: float = 1.0):
+        super(Rotate, self).__init__(name=name)
+        self.topic_name = topic_name
+        self.counterclockwise = counterclockwise
+        self.vel = vel
+
+    def setup(self, **kwargs):
+        self.logger.debug("{}.setup()".format(self.qualified_name))
+        try:
+            self.node = kwargs['node']
+        except KeyError as e:
+            error_message = "didn't find 'node' in setup's kwargs [{}][{}]".format(
+                self.qualified_name)
+            raise KeyError(error_message) from e  # 'direct cause' traceability
+
+        self.publisher = self.node.create_publisher(
             msg_type=geometry_msgs.Twist,
             topic=self.topic_name,
             qos_profile=py_trees_ros.utilities.qos_profile_latched()
-		)
-		self.feedback_message = "publisher created"
+        )
+        self.feedback_message = "publisher created"
 
-	def update(self) -> py_trees.common.Status:
-		self.logger.debug("%s.update()" % self.__class__.__name__)
-		msg = geometry_msgs.Twist()
-		msg.angular.z = self.vel * ((-1)** (1*(not self.counterclockwise)))
-		self.publisher.publish(msg)
-		self.feedback_message = "Publishing rotation:" + str(msg.angular.z)
-		return py_trees.common.Status.SUCCESS
-	
-	def terminate(self, new_status: py_trees.common.Status) -> None:
-		self.logger.debug(
+    def update(self) -> py_trees.common.Status:
+        self.logger.debug("%s.update()" % self.__class__.__name__)
+        msg = geometry_msgs.Twist()
+        msg.angular.z = self.vel * ((-1) ** (1*(not self.counterclockwise)))
+        self.publisher.publish(msg)
+        self.feedback_message = "Publishing rotation:" + str(msg.angular.z)
+        return py_trees.common.Status.SUCCESS
+
+    def terminate(self, new_status: py_trees.common.Status) -> None:
+        self.logger.debug(
             "{}.terminate({})".format(
                 self.qualified_name,
-                "{}->{}".format(self.status, new_status) if self.status != new_status else "{}".format(new_status)
+                "{}->{}".format(self.status,
+                                new_status) if self.status != new_status else "{}".format(new_status)
             )
-		)
+        )
+
+
+class Drive(py_trees.behaviour.Behaviour):
+    def __init__(self,
+                 name: str,
+                 topic_name: str = "/cmd_vel",
+                 vel: float = 0.12):
+        super(Drive, self).__init__(name=name)
+        self.topic_name = topic_name
+        self.vel = vel
+
+    def setup(self, **kwargs):
+        self.logger.debug("{}.setup()".format(self.qualified_name))
+        try:
+            self.node = kwargs['node']
+        except KeyError as e:
+            error_message = "didn't find 'node' in setup's kwargs [{}][{}]".format(
+                self.qualified_name)
+            raise KeyError(error_message) from e  # 'direct cause' traceability
+
+        self.publisher = self.node.create_publisher(
+            msg_type=geometry_msgs.Twist,
+            topic=self.topic_name,
+            qos_profile=py_trees_ros.utilities.qos_profile_latched()
+        )
+        self.feedback_message = "publisher created"
+
+    def update(self) -> py_trees.common.Status:
+        self.logger.debug("%s.update()" % self.__class__.__name__)
+        msg = geometry_msgs.Twist()
+        msg.linear.x = self.vel
+        self.publisher.publish(msg)
+        self.feedback_message = "Publishing linear move:" + str(msg.linear.x)
+        return py_trees.common.Status.SUCCESS
+
+    def terminate(self, new_status: py_trees.common.Status) -> None:
+        self.logger.debug(
+            "{}.terminate({})".format(
+                self.qualified_name,
+                "{}->{}".format(self.status,
+                                new_status) if self.status != new_status else "{}".format(new_status)
+            )
+        )
--- bt_scripts/run_bt.py
+++ bt_scripts/run_bt.py
@@ -8,10 +8,11 @@ import behaviours
 from py_trees_ros import subscribers as py_trees_subscribers
 import sensor_msgs.msg as sensor_msgs
 
+
 def create_root() -> py_trees.behaviour.Behaviour:
-    # Kompozycja typu Parallel, będzie korzeniem naszego drzewa. 
+    # Kompozycja typu Parallel, będzie korzeniem naszego drzewa.
     # Wszyskie połączone do niego dzieci będą wykonywane równocześnie
-    root = py_trees.composites.Parallel( 
+    root = py_trees.composites.Parallel(
         name="Turtlebot3 Behaviour Tree",
         policy=py_trees.common.ParallelPolicy.SuccessOnAll(
             synchronise=True
@@ -19,39 +20,58 @@ def create_root() -> py_trees.behaviour.Behaviour:
     )
 
     # zachowanie Rotate, powoduje obrót robota w miejscu.
-    rotation = behaviours.Rotate(name="RotateRobot") 
+    rotation = behaviours.Rotate(name="RotateRobot")
 
-    # TODO: Utworzenie zachowania Drive (należy zaimplementować), które wykonuje ruch do przodu.
-    # driving = behaviours.Drive()
-    
-    # zachowanie subskrybujące skan lasera z robota i przekazujące 
+    # Utworzenie zachowania Drive (należy zaimplementować), które wykonuje ruch do przodu.
+    driving = behaviours.Drive(name="DriveRobot")
+
+    # zachowanie subskrybujące skan lasera z robota i przekazujące
     # go do Tablicy (przekazuje wyłącznie pole "ranges")
     laser2BB = py_trees_subscribers.ToBlackboard(name="Laser2BB",
                                                  topic_name="/scan",
                                                  topic_type=sensor_msgs.LaserScan,
-                                                 blackboard_variables={'/scan': 'ranges'},
-                                                 qos_profile=py_trees_ros.utilities.qos_profile_unlatched(), 
+                                                 blackboard_variables={
+                                                     '/scan': 'ranges'},
+                                                 qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
                                                  clearing_policy=py_trees.common.ClearingPolicy.NEVER)
-    
+
     # funkcja sprawdzająca czy odległość do przeszkody jest większa niż 3 metry
-    # musi odczytać wartości skanera zapisane w Tablicy 
+    # musi odczytać wartości skanera zapisane w Tablicy
     # (tablica jest domyślnie przekazywana przez EternalGuard)
     def check_distance_in_front(blackboard: py_trees.blackboard.Blackboard) -> bool:
         DISTANCE_THRESHOLD = 3
         scan = blackboard.scan
+        # take only +- 10 degrees from the front
+        scan = scan[0:10] + scan[-10:]
+        # check if in the scan there is a distance larger than 3 meters
+        for distance in scan:
+            if distance > DISTANCE_THRESHOLD:
+                return True
         print(scan)
         # TODO: Sprawdzanie czy dystans jest większy niż 3 metry.
         return False
-    
-    # TODO: Dodanie dekoratora EternalGuard z warunkiem dystansu do przeszkody do jazdy na wprost.
-    # distance_check = py_trees.decorators.EternalGuard() 
-    
-    # TODO: Modyfikacja drzewa tak, żeby było możliwe realizowanie założeń z instrukcji.
+
+    # Dodanie dekoratora EternalGuard z warunkiem dystansu do przeszkody do jazdy na wprost.
+    distance_check = py_trees.decorators.EternalGuard(
+        name="DistanceCheck",
+        child=driving,
+        condition=check_distance_in_front,
+        blackboard_keys={"scan"}
+    )
+    failure_selector = py_trees.composites.Selector(name="FailureSelector")
+    failure_selector.add_child(py_trees.behaviours.Failure(name="Failure"))
+    failure_selector.add_child(rotation)
+    # Modyfikacja drzewa tak, żeby było możliwe realizowanie założeń z instrukcji.
     root.add_child(laser2BB)
     root.add_child(rotation)
-    
+    root.add_child(py_trees.decorators.Selector(
+        name="DistanceCheckSelector",
+        children=[distance_check, failure_selector]
+    ))
+
     # Zwracanie korzenia i połączonych do niego dzieci
     return root
+
 
 def main():
     rclpy.init()
@@ -66,7 +86,8 @@ def main():
     try:
         tree.setup(timeout=15)
     except py_trees_ros.exceptions.TimedOutError as e:
-        console.logerror(console.red + "failed to setup the tree, aborting [{}]".format(str(e)) + console.reset)
+        console.logerror(
+            console.red + "failed to setup the tree, aborting [{}]".format(str(e)) + console.reset)
         tree.shutdown()
         rclpy.try_shutdown()
         sys.exit(1)
@@ -88,6 +109,7 @@ def main():
     finally:
         tree.shutdown()
         rclpy.try_shutdown()
+
 
 if __name__ == '__main__':
     main()
